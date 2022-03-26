b0d816f7e3a2e54d199215261a58d4f0f996cc88
Alberto Del Águila <nautadev@gmail.com>
2022-03-26 14:55:20 +0100

    Updated GA4

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,10 +4,13 @@ import { useEffect } from 'react';
 import TagManager from 'react-gtm-module/dist/TagManager';
 import '../style.global.css';
 import { GTM_ID } from '../config';
+import ReactGA from "react-ga4";
 
 const Website = ({ Component, pageProps }) => {
   useEffect(()=>{
       TagManager.initialize({gtmId: GTM_ID});
+      ReactGA.initialize("G-VV5KC401XE");
+      ReactGA.send("pageview");
   }, []);
 
   return (

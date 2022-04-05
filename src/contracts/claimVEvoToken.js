d372e34c5179fb7695007975608e48b7986baa27
Alberto Del Águila <nautadev@gmail.com>
2022-04-05 22:55:04 +0200

    Updated

--- src/contracts/claimVEvoToken.js
+++ src/contracts/claimVEvoToken.js
@@ -5,7 +5,7 @@ import _testnet from '../abis/testnet/claimVEvoToken.json';
 
 const address = isDevMode ? "0x8229190ada4a39cb7be14d6ff60f43029852adde" : "0x7a82dab3c5e53895ED080307Db8d1e1b8B903A79";
 
-const abi = isDevMode ? _testnet.abi : [];
+const abi = _testnet.abi;
 
 const contract = (web3) => {
     if (web3) {

8803276f1565c895346be4e2d8d195bcf26a2d5e
Alberto Del Águila <nautadev@gmail.com>
2022-04-05 22:49:31 +0200

    Updated

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,9 @@
 export const GTM_ID = 'GTM-M2BWXLW';
-const DEV = true;
+const DEV = false;
 const HARMONY_TESTNET = 1666700000;
 const HARMONY_MAINNET = 1666600000;
 const VEVO_TOKEN_TESTNET = "0xf7979fe14b786e4718714d7bf91cbe8ae21359ce";
-const VEVO_TOKEN_MAINET = "";
+const VEVO_TOKEN_MAINET = "0xEb76Ef5d121f31c2cb59e50A4fa5475042C84e34";
 
 export const isDevMode = () => DEV;
 export const getChainID = () => DEV ? HARMONY_TESTNET : HARMONY_MAINNET;
--- src/contracts/claimVEvoToken.js
+++ src/contracts/claimVEvoToken.js
@@ -3,7 +3,7 @@ import { isDevMode } from '../config';
 
 import _testnet from '../abis/testnet/claimVEvoToken.json';
 
-const address = isDevMode ? "0x8229190ada4a39cb7be14d6ff60f43029852adde" : "";
+const address = isDevMode ? "0x8229190ada4a39cb7be14d6ff60f43029852adde" : "0x7a82dab3c5e53895ED080307Db8d1e1b8B903A79";
 
 const abi = isDevMode ? _testnet.abi : [];
 
